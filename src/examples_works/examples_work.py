232615fd9a51be911a6b38871deeefad0654524a
KusokTatarina <nickiforov.ni2013@yandex.ru>
2024-04-18 18:15:17 +0300

    Nu teper tochno rabotaet

--- src/examples_works/examples_work.py
+++ src/examples_works/examples_work.py
@@ -1,16 +1,16 @@
 from telebot.types import InputMediaPhoto
 
 def group_my_examples_work1():
-        pic2 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\2.jpg", "rb")
-        pic3 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\3.jpg", "rb")
-        pic4 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\4.jpg", "rb")
-        pic5 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\5.jpg", "rb")
-        pic6 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\6.jpg", "rb")
-        pic7 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\7.jpg", "rb")
-        pic8 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\8.jpg", "rb")
-        pic9 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\9.jpg", "rb")
-        pic10 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\10.jpg", "rb")
-        pic11 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\11.jpg", "rb")
+        pic2 = open(r"examples_works/2.jpg", "rb")
+        pic3 = open(r"examples_works/3.jpg", "rb")
+        pic4 = open(r"examples_works/4.jpg", "rb")
+        pic5 = open(r"examples_works/5.jpg", "rb")
+        pic6 = open(r"examples_works/6.jpg", "rb")
+        pic7 = open(r"examples_works/7.jpg", "rb")
+        pic8 = open(r"examples_works/8.jpg", "rb")
+        pic9 = open(r"examples_works/9.jpg", "rb")
+        pic10 = open(r"examples_works/10.jpg", "rb")
+        pic11 = open(r"examples_works/11.jpg", "rb")
 
         
         media = [                               
@@ -30,16 +30,16 @@ from telebot.types import InputMediaPhoto
 
 def group_my_examples_work2():
 
-        pic12 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\12.jpg", "rb")
-        pic13 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\13.jpg", "rb")
-        pic14 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\14.jpg", "rb")
-        pic15 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\15.jpg", "rb")
-        pic16 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\16.jpg", "rb")
-        pic17 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\17.jpg", "rb")
-        pic18 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\18.jpg", "rb")
-        pic19 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\19.jpg", "rb")
-        pic20 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\20.jpg", "rb")
-        pic21 = open(r"C:\Users\nicki\Desktop\LuciviyBOT\src\examples_works\21.jpg", "rb")
+        pic12 = open(r"examples_works/12.jpg", "rb")
+        pic13 = open(r"examples_works/13.jpg", "rb")
+        pic14 = open(r"examples_works/14.jpg", "rb")
+        pic15 = open(r"examples_works/15.jpg", "rb")
+        pic16 = open(r"examples_works/16.jpg", "rb")
+        pic17 = open(r"examples_works/17.jpg", "rb")
+        pic18 = open(r"examples_works/18.jpg", "rb")
+        pic19 = open(r"examples_works/19.jpg", "rb")
+        pic20 = open(r"examples_works/20.jpg", "rb")
+        pic21 = open(r"examples_works/21.jpg", "rb")
 
         
         media = [
